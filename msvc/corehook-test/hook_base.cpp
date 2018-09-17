72b7c3e53cbf0e7ef08d42c7fa5685b2d8b5be3b
unknown v2 <unknownv2@users.noreply.github.com>
2018-09-16 21:07:38 -0400

    Cleanup base test code

--- msvc/corehook-test/hook_base.cpp
+++ msvc/corehook-test/hook_base.cpp
@@ -2,22 +2,20 @@
 #include "detours.h"
 
 bool detoured_test = false;
-#pragma optimize( "", off )
-unsigned int OriginalFunction2(unsigned int count) {
-    int testValue1 = count * 4;
-    int testValue2 = count * 10;
 
-    return (testValue2 *testValue1) + count + 1;
-}
+// disable optimizations for test functions so they aren't inlined
+#pragma optimize( "", off )
+
 unsigned int OriginalFunction(unsigned int count) {
-    void * mem = malloc(0x100);
-    return (ULONG_PTR)mem + count + 1;
+
+    return count + 1;
 }
 unsigned int DetourForOriginalFunction(unsigned int count) {
     detoured_test = true;
 
     return OriginalFunction(count);
 }
+
 #pragma optimize( "", on )
 
 bool HookTest()
