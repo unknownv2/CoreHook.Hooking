cf51871103c628791a5779c65154a7d9efcadc90
unknown v2 <unknownv2@users.noreply.github.com>
2018-09-16 21:01:45 -0400

    Check return value of DetourInstallHook in test

--- msvc/corehook-test/hook_base.cpp
+++ msvc/corehook-test/hook_base.cpp
@@ -2,15 +2,24 @@
 #include "detours.h"
 
 bool detoured_test = false;
+#pragma optimize( "", off )
+unsigned int OriginalFunction2(unsigned int count) {
+    int testValue1 = count * 4;
+    int testValue2 = count * 10;
 
+    return (testValue2 *testValue1) + count + 1;
+}
 unsigned int OriginalFunction(unsigned int count) {
-    return count + 1;
+    void * mem = malloc(0x100);
+    return (ULONG_PTR)mem + count + 1;
 }
 unsigned int DetourForOriginalFunction(unsigned int count) {
     detoured_test = true;
 
     return OriginalFunction(count);
 }
+#pragma optimize( "", on )
+
 bool HookTest()
 {
     DetourBarrierProcessAttach();
@@ -20,17 +29,17 @@ bool HookTest()
     LONG callback = 0;
     TRACED_HOOK_HANDLE hookHandle = new HOOK_TRACE_INFO();
 
-    DetourInstallHook((void*)OriginalFunction, (void*)DetourForOriginalFunction,
+   LONG error = DetourInstallHook((void*)OriginalFunction, (void*)DetourForOriginalFunction,
         &callback, hookHandle);
+   if (error == NO_ERROR) {
+       DetourSetInclusiveACL(new ULONG(), 1, hookHandle);
 
-    DetourSetInclusiveACL(new ULONG(), 1, hookHandle);
+       OriginalFunction(1);
 
-    OriginalFunction(1);
+       DetourUninstallHook(hookHandle);
 
-    DetourUninstallHook(hookHandle);
-
-    DetourBarrierProcessDetach();
-    DetourCriticalFinalize();
-
-    return detoured_test;
+       DetourBarrierProcessDetach();
+       DetourCriticalFinalize();
+   }
+   return detoured_test;
 }
