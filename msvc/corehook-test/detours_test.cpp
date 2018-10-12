49ef500e88afbb418c7ea38dbce6e7a2226a9c9a
Thierry <unknownv2@users.noreply.github.com>
2018-10-12 19:08:51 -0400

    test: Add tests for invalid parameters for the  function DetourGetHookBypassAddress

--- msvc/corehook-test/detours_test.cpp
+++ msvc/corehook-test/detours_test.cpp
@@ -80,3 +80,10 @@ TEST_F(DetoursTest, ShouldFailWhenInstallingMaxHookCount) {
         }
     }
 }
+TEST_F(DetoursTest, GetHookBypassAddress_Should_Return_Invalid_Handle_With_Bad_Hook_Handle) {
+    EXPECT_EQ(ERROR_INVALID_HANDLE, DetourGetHookBypassAddress(nullptr, nullptr));
+}
+TEST_F(DetoursTest, GetHookBypassAddress_Should_Return_Invalid_Handle_With_Bad_Output_Address) {
+    HOOK_TRACE_INFO pHandle;
+    EXPECT_EQ(ERROR_INVALID_PARAMETER, DetourGetHookBypassAddress(&pHandle, nullptr));
+}
